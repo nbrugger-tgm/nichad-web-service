a91b117c77409002d459dbeef1834a07824bd0a6
nbrugger-tgm <nbrugger@student.tgm.ac.at>
2020-12-22 13:40:43 +0100

    Spring init

---
+++ src/main/java/com/niton/jchad/NiChadApplication.java
@@ -0,0 +1,13 @@
+package com.niton.jchad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NiChadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NiChadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/niton/jchad/NiChadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niton.jchad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NiChadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
