f2f3962a4a293f9572baad37bc6b7b3559287416
nbrugger-tgm <nbrugger@student.tgm.ac.at>
2020-12-22 14:27:49 +0100

    Add JPA Repos

---
+++ src/main/java/com/niton/jchad/jpa/UserRepo.java
@@ -0,0 +1,8 @@
+package com.niton.jchad.jpa;
+
+import com.niton.jchad.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepo extends JpaRepository<User,String> {}
