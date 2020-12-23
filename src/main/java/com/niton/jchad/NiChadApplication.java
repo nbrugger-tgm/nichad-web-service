c200cbb4ca5e9389d05d7602a0ac7bb98520856f
nbrugger-tgm <nbrugger@student.tgm.ac.at>
2020-12-23 10:41:42 +0100

    Patch version

--- src/main/java/com/niton/jchad/NiChadApplication.java
+++ src/main/java/com/niton/jchad/NiChadApplication.java
@@ -1,12 +1,18 @@
 package com.niton.jchad;
 
+import com.niton.util.Logging;
+import com.niton.util.config.Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.ObjectInputFilter;
 
 @SpringBootApplication
 public class NiChadApplication {
 
 	public static void main(String[] args) {
+		Config.init("config.cfg");
+		Logging.init("web-service", "logging");
 		SpringApplication.run(NiChadApplication.class, args);
 	}
 
